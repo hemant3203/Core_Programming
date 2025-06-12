4a43ae5c91a337370baff9af41627a5c16dbbd3a
hemant3203 <prajapatihero341@gmail.com>
2025-06-12 11:26:11 +0530

    question1

--- Level_01/q1.java
+++ Level_01/q1.java
@@ -1,3 +1,4 @@
+
 public class q1{
     public static void main(String[] args) {
         int birthYear = 2000;
---
+++ Practice_problem_01/q1.java
@@ -0,0 +1,12 @@
+package Practice_problem_01;
+import java.util.*;
+public class q1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int math=94;
+        int physics=95;
+        int chemistry=96;
+        System.out.println("Sam average mark in PCM is"+(double) (math+physics+chemistry)/3.0);
+        sc.close();
+    }
+}
