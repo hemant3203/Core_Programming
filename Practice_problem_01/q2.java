2e7bd6c47afc027b2921d0dd0aa8b7107fd0ae2f
hemant3203 <prajapatihero341@gmail.com>
2025-06-12 11:44:30 +0530

    question number 2

---
+++ Practice_problem_01/q2.java
@@ -0,0 +1,11 @@
+package Practice_problem_01;
+import java.util.*;
+public class q2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+         int pen=14;
+         int students=3;
+         System.out.println("The pen per students is "+ pen/students+ "and the remaining pen not distributed is " +pen%students);
+         sc.close();
+    }
+}
