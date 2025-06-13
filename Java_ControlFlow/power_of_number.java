984bf70191908a732f8ba318e6b0b0847af0c459
hemant3203 <prajapatihero341@gmail.com>
2025-06-13 23:33:04 +0530

    [Hemant Prajapati] Add. power of n

---
+++ Java_ControlFlow/power_of_number.java
@@ -0,0 +1,16 @@
+package Java_ControlFlow;
+import java.util.*;
+public class power_of_number {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int number=sc.nextInt();
+        int power=sc.nextInt();
+        int result=1,counter=0;
+           while(counter!=power){
+            result=result*number;
+            counter++;
+           }
+           System.out.println(result);
+           sc.close();
+    }
+}
