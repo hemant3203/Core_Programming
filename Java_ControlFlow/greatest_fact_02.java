d57374c4b70849543547128f59cef33120b2ad66
hemant3203 <prajapatihero341@gmail.com>
2025-06-14 10:58:59 +0530

    [Hemant Prajapati] Add. greatest factor using for loop

---
+++ Java_ControlFlow/greatest_fact_02.java
@@ -0,0 +1,22 @@
+package Java_ControlFlow;
+import java.util.*;
+
+public class greatest_fact_02{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int greatestFactor = 1;
+        int counter = number - 1;
+
+        while (counter >= 1) {
+            if (number % counter == 0) {
+                greatestFactor = counter;
+                break;
+            }
+            counter--;
+        }
+
+        System.out.println(greatestFactor);
+        sc.close();
+    }
+}
