7c580c615efd21a3a9014c679d5b7095a8a0e7ad
hemant3203 <prajapatihero341@gmail.com>
2025-06-14 11:26:16 +0530

    [Hemant Prajapati] Add. harshad Number

---
+++ Java_ControlFlow/harshad_check.java
@@ -0,0 +1,24 @@
+package Java_ControlFlow;
+import java.util.*;
+
+public class harshad_check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int sum = 0;
+        int temp = number;
+
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        if (number % sum == 0) {
+            System.out.println("Harshad Number");
+        } else {
+            System.out.println("Not a Harshad Number");
+        }
+
+        sc.close();
+    }
+}
