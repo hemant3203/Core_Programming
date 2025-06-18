454d3e20804bd6eeee2361d2e17fae0bf89071cb
hemant3203 <prajapatihero341@gmail.com>
2025-06-18 05:57:59 +0530

    [Hemant Prajapati Add. Number Type Checker

---
+++ Methods/NumberTypeChecker.java
@@ -0,0 +1,57 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class NumberTypeChecker {
+
+    public static boolean isPerfect(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) sum += i;
+        }
+        return sum == n;
+    }
+
+    public static boolean isAbundant(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) sum += i;
+        }
+        return sum > n;
+    }
+
+    public static boolean isDeficient(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) sum += i;
+        }
+        return sum < n;
+    }
+
+    public static boolean isStrong(int n) {
+        int sum = 0, temp = n;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == n;
+    }
+
+    public static int factorial(int n) {
+        int fact = 1;
+        for (int i = 2; i <= n; i++) fact *= i;
+        return fact;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println("Perfect Number: " + isPerfect(num));
+        System.out.println("Abundant Number: " + isAbundant(num));
+        System.out.println("Deficient Number: " + isDeficient(num));
+        System.out.println("Strong Number: " + isStrong(num));
+        sc.close();
+    }
+}
