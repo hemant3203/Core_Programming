00908b8391cc00220f9bd6aa61e3a73c4c60a838
hemant3203 <prajapatihero341@gmail.com>
2025-06-19 15:19:54 +0530

    [Hemant Prajapati] Add. Time Zones

---
+++ Built_in_Functions/TimeZones.java
@@ -0,0 +1,24 @@
+package Built_in_Functions;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class TimeZones{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+
+        System.out.println("Current time in GMT: " + gmtTime.format(formatter));
+        System.out.println("Current time in IST: " + istTime.format(formatter));
+        System.out.println("Current time in PST: " + pstTime.format(formatter));
+
+        sc.close();
+    }
+}
