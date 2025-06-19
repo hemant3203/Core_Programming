c3a6f6c1a112ae0febad1768bedbe693a9a60676
hemant3203 <prajapatihero341@gmail.com>
2025-06-19 15:25:20 +0530

    [Hemant Prajapati] Add. Printing Date in Three different Format

---
+++ Built_in_Functions/DateFormatting.java
@@ -0,0 +1,23 @@
+package Built_in_Functions;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DateFormatting{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        LocalDate currentDate = LocalDate.now();
+
+        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter format2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter format3 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+
+        System.out.println("Date in dd/MM/yyyy format: " + currentDate.format(format1));
+        System.out.println("Date in yyyy-MM-dd format: " + currentDate.format(format2));
+        System.out.println("Date in EEE, MMM dd, yyyy format: " + currentDate.format(format3));
+
+        sc.close();
+    }
+}
