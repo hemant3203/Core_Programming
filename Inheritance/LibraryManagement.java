85e218c8c4b33e17629284b8befb3ff352ea2d24
hemant3203 <prajapatihero341@gmail.com>
2025-07-01 23:58:04 +0530

    [Hemant Prajapati] Add. Smart Home Devives

---
+++ Inheritance/LibraryManagement.java
@@ -0,0 +1,46 @@
+package Inheritance;
+
+import java.util.Scanner;
+
+class Book {
+    String title;
+    int publicationYear;
+
+    Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    void displayInfo() {
+        System.out.println("Title: " + title + ", Year: " + publicationYear);
+    }
+}
+
+class Author extends Book {
+    String name;
+    String bio;
+
+    Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author: " + name);
+        System.out.println("Bio: " + bio);
+    }
+}
+
+public class LibraryManagement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+         Author book1 = new Author("The Art of Being Alone", 2020, "Renuka Gavrani", "Indian author known for writings on solitude and inner strength.");
+        book1.displayInfo();
+
+        sc.close();
+    }
+}
