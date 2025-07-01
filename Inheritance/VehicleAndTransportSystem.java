5e367486dc58e0f15c2e371720aa2fdd2071b9de
hemant3203 <prajapatihero341@gmail.com>
2025-07-01 23:52:24 +0530

    [Hemant Prajapati] Add. Different Vehicles hierarchy

---
+++ Inheritance/VehicleAndTransportSystem.java
@@ -0,0 +1,80 @@
+package Inheritance;
+
+import java.util.Scanner;
+
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed + " km/h, Fuel Type: " + fuelType);
+    }
+}
+
+class Car extends Vehicle {
+    int seatCapacity;
+
+    Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+class Truck extends Vehicle {
+    double loadCapacity;
+
+    Truck(int maxSpeed, String fuelType, double loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    boolean hasCarrier;
+
+    Motorcycle(int maxSpeed, String fuelType, boolean hasCarrier) {
+        super(maxSpeed, fuelType);
+        this.hasCarrier = hasCarrier;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Carrier: " + (hasCarrier ? "Yes" : "No"));
+    }
+}
+
+public class VehicleAndTransportSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 10);
+        vehicles[2] = new Motorcycle(150, "Petrol", true);
+
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
