396b81a6c17412b62ac9c4e7b9334b720c6a5a50
hemant3203 <prajapatihero341@gmail.com>
2025-07-12 20:21:00 +0530

    [Hemant Prajapati] Add. Read File line by line

---
+++ FileHandling/ReadFileLineByLine.java
@@ -0,0 +1,26 @@
+package FileHandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileLineByLine {
+    public static void main(String[] args) {
+        String filePath = "input.txt";
+
+        try {
+            FileReader fr = new FileReader(filePath);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
