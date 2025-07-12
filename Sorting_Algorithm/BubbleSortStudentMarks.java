7eca58225ab8a18b09242e5fd65876fc932e021a
hemant3203 <prajapatihero341@gmail.com>
2025-07-12 19:16:24 +0530

    [Hemant Prajapati] Bubble sorting

---
+++ Sorting_Algorithm/BubbleSortStudentMarks.java
@@ -0,0 +1,35 @@
+package Sorting_Algorithm;
+
+import java.util.Scanner;
+
+public class BubbleSortStudentMarks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] marks = new int[n];
+        for (int i = 0; i < n; i++) {
+            marks[i] = sc.nextInt();
+        }
+
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (marks[j] > marks[j + 1]) {
+                    int temp = marks[j];
+                    marks[j] = marks[j + 1];
+                    marks[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(marks[i] + " ");
+        }
+        sc.close();
+    }
+}
