39ecfae878ae3d4bbbc2e146aa5baaa63baa0e96
hemant3203 <prajapatihero341@gmail.com>
2025-07-12 19:45:33 +0530

    [Hemant Prajapati] Add. Count Sort

---
+++ Sorting_Algorithm/CountingSortStudentAges.java
@@ -0,0 +1,38 @@
+package Sorting_Algorithm;
+
+import java.util.Scanner;
+
+public class CountingSortStudentAges {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ages = new int[n];
+        for (int i = 0; i < n; i++) {
+            ages[i] = sc.nextInt();
+        }
+
+        int min = 10;
+        int max = 18;
+        int range = max - min + 1;
+        int[] count = new int[range];
+        int[] output = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            count[ages[i] - min]++;
+        }
+
+        for (int i = 1; i < range; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[ages[i] - min] - 1] = ages[i];
+            count[ages[i] - min]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(output[i] + " ");
+        }
+        sc.close();
+    }
+}
