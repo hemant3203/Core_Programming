51df9986d9eb2d9ab90ac05cca179367c2271515
hemant3203 <prajapatihero341@gmail.com>
2025-07-12 23:14:56 +0530

    [Hemant Prajapati] Add. Find first and Last Occurence

---
+++ Searching/FirstAndLastOccurrence.java
@@ -0,0 +1,71 @@
+package Searching;
+
+import java.util.Scanner;
+
+public class FirstAndLastOccurrence {
+
+    public static int findFirst(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                right = mid - 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static int findLast(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                left = mid + 1;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int first = findFirst(array, target);
+        int last = findLast(array, target);
+
+        if (first == -1) {
+            System.out.println("-1");
+        } else {
+            System.out.println(first + " " + last);
+        }
+
+        sc.close();
+    }
+}
