cecf9cdb5f7fe6b53798e71dab38214126f7d5c5
hemant3203 <prajapatihero341@gmail.com>
2025-07-12 23:01:17 +0530

    [Hemant Prajapati] Add. First Negative number

---
+++ Searching/FirstNegativeIndex.java
@@ -0,0 +1,28 @@
+package Searching;
+
+import java.util.Scanner;
+
+public class FirstNegativeIndex {
+    public static int findFirstNegative(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        int index = findFirstNegative(array);
+        System.out.println(index);
+        sc.close();
+    }
+}
